b678b2a2b20da39a5f909d6086aa2af034707a24
nhuthuy <nhuthuy2972@gmail.com>
2020-06-28 01:05:13 +0700

    fix select conditions

--- api/device.js
+++ api/device.js
@@ -4,5 +4,5 @@ const router = express.Router()
 const DeviceController = require('../controllers/device')
 router.post('/getdata', DeviceController.getdata)
 router.post('/insertdata', DeviceController.insertdata)
-
+router.get('/insertdata1', DeviceController.insert1)
 module.exports = router
--- controllers/device.js
+++ controllers/device.js
@@ -6,16 +6,51 @@ const pool = new Pool({
 })
 
 
+exports.insert1 = async (req, res) => {
+    let minutes = 15
+    let t = 0
+    let ph = 0
+    let h = 0
+    let d = 0
+    try {
+        for (let i = 1; i < 100; i++) {
+            t = Math.floor(Math.random() * (45 - 25)) + 25;
+            ph = Math.floor(Math.random() * (12 - 2)) + 2;
+            h = Math.floor(Math.random() * (85 - 45)) + 45;
+            d = Math.floor(Math.random() * (15 - 5)) + 5;
+            await pool.query(
+                `insert into metrics (time,device_id,data) values(now() - interval '${minutes} minutes','fqH8JpUdYoT4yfJc_v8ZH','{"temperature" : ${t} , "ph": ${ph} , "humidity": ${h} ,"do": ${d}}')`)
+
+            minutes += 15;
+        }
+
+        //console.log(rows)
+        res.json({
+            messages: "insert successfuly"
+        })
+    } catch (error) {
+        res.json({
+            error
+        })
+    }
+}
+
+
 exports.getdata = async (req, res) => {
     const device_id = req.body.device_id
-    console.log(device_id)
+    const timestamp = req.body.timestamp
+    console.log(timestamp)
+    //let date = new Date(0)
     try {
         const { rows } =
             await pool.query(
-                `select time,data from metrics where device_id = '${device_id}' order by time desc limit 24`)
+                `select time,data from metrics where device_id = '${device_id}' and time > timestamp with time zone '${timestamp}' - interval '12 hours' order by time desc`)
         console.log(rows)
 
-        let result = rows.map(row => { return { time: moment(row.time).format("[Lúc] hh:mm:ss a, DD/MM/YYYY"), temperature: row.data.temperature, humidity: row.data.humidity } })
+        let result = rows.map(row => {
+            return { ...row.data, time: moment(row.time).format("hh:mm a DD/MM/YY") }
+        })
+
         console.log(result.reverse())
         res.json({
             rows: [...result]
@@ -46,7 +81,8 @@ exports.insertdata = async (req, res) => {
     // console.log(timestamp)
     // console.log(device_id)
     try {
-        const { rows } = await pool.query(
+
+        let { rows } = await pool.query(
             `insert into metrics (time,device_id,data) values('${timestamp}','${device_id}','${data}')`)
         console.log(rows)
         res.json({
